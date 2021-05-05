2d092c872175e9f517c0110e2c7ad17e15ee8e0e
Wawa <wawamama@gmail.com>
2021-05-06 00:04:50 +0200

    just no flip

--- countdown.js
+++ countdown.js
@@ -1,6 +1,7 @@
 const countdownDate = new Date().getTime() + (7 * 24 * 60 * 60 * 1000); // 7 days from now
+let flipcards = document.getElementsByClassName('top-case');
 
-let countdown = () => {
+const countdown = () => {
     let now = new Date().getTime();
     let interval = countdownDate - now;
     let days = Math.floor(interval / (1000 * 60 * 60 * 24));
@@ -14,10 +15,17 @@ let countdown = () => {
     document.getElementById("counting-minutes").innerHTML = minutes;
     document.getElementById("counting-seconds").innerHTML = seconds;
 
+    // Flip
+    flipcards[3].classList.add('flip');
+
     if (interval < 0) {
         clearInterval(countdown);
         document.getElementById("finito-div").innerHTML = "FINITO!";
       }
+}
+
+const flip = (div) => {
+
 }
 
 // Execute countdown function every 1sec
