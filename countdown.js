eea38269b582e287d87358b9319273af8882a42a
Wawa <wawamama@gmail.com>
2021-05-05 22:29:49 +0200

    not responsive yet

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
-const countdownDate = new Date('Jan 1, 2022 00:00:01').getTime();
+const countdownDate = new Date().getTime() + (7 * 24 * 60 * 60 * 1000); // 
 
 let countdown = () => {
-    let now = new Date.getTime();
+    let now = new Date().getTime();
     let interval = countdownDate - now;
     let days = Math.floor(interval / (1000 * 60 * 60 * 24));
     let hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -22,3 +22,5 @@ let countdown = () => {
 
 // Execute countdown function every 1sec
 window.setInterval(countdown, 1000);
+
+console.log('hey')
